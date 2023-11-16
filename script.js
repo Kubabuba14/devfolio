9abe3a06ba3234cbb2b0dc9b4f8a12f00e00b2cb
Kubabuba14 <jakubwit@hotmail.co.uk>
2023-11-16 19:53:18 +0100

    js try

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
-// Array of project details
 const projects = [
     { id: 1, imageURL: 'https://unsplash.com/photos/-5rA4DRrEXU/download?force=true' },
     { id: 23, imageURL: 'https://unsplash.com/photos/ZO4rHqkCat4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fHNwbGFzaHxlbnwwfHx8fDE3MDAxNTAzODl8MA&force=true' },
-];
-
-// Function to create a project element
-function createProjectElement(project) {
+    // Add more project details as needed
+  ];
+  
+  function createProjectElement(project) {
     const projectElement = document.createElement('div');
     projectElement.className = 'project';
     projectElement.id = `project${project.id}`;
-
-    const projectOverlay = document.createElement('div');
-    projectOverlay.className = 'project-overlay';
-    projectOverlay.style.background = 'linear-gradient(rgba(17, 17, 24, 0.1), rgba(17, 17, 24, 0.9))';
-
+  
     const projectImage = document.createElement('div');
     projectImage.style.backgroundImage = `url(${project.imageURL})`;
     projectImage.className = 'project-image';
-
-    const projectTitle = document.createElement('h2');
-    projectTitle.textContent = `Project ${project.id}`;
-
-    projectElement.appendChild(projectOverlay); // Add the overlay
+  
+    // Add overlay styling
+    projectElement.style.position = 'relative';
+    projectElement.style.zIndex = '1';
+    projectElement.style.overflow = 'hidden';
+  
+    const overlay = document.createElement('div');
+    overlay.style.content = '';
+    overlay.style.position = 'absolute';
+    overlay.style.top = '0';
+    overlay.style.right = '0';
+    overlay.style.bottom = '0';
+    overlay.style.left = '0';
+    overlay.style.background = 'linear-gradient(rgba(17, 17, 24, 0.1), rgba(17, 17, 24, 0.9))';
+    overlay.style.zIndex = '1';
+  
     projectElement.appendChild(projectImage);
-    projectElement.appendChild(projectTitle);
-
+    projectElement.appendChild(overlay);
+  
     return projectElement;
-}
-
-// Function to add projects to the projects container
-function addProjectsToContainer() {
+  }
+  
+  function addProjectsToContainer() {
     const projectsContainer = document.querySelector('.projects-container');
-
+  
     projects.forEach((project) => {
-        const projectElement = createProjectElement(project);
-        projectsContainer.appendChild(projectElement);
+      const projectElement = createProjectElement(project);
+      projectsContainer.appendChild(projectElement);
     });
-}
-
-// Call the function to add projects to the container
-addProjectsToContainer();
-
-
+  }
+  
+  addProjectsToContainer();
+  
